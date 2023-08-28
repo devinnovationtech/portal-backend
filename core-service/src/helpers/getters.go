bfe1ce24cbe3af6a041200a2987e178e1e169aba
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2023-08-28 13:22:03 +0700

    core-svc-feat(helper): add method for get init log (#529)

--- core-service/src/helpers/getters.go
+++ core-service/src/helpers/getters.go
@@ -2,12 +2,14 @@ package helpers
 
 import (
 	"encoding/json"
+	"fmt"
 	"math"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
+	_goLog "github.com/jabardigitalservice/golog/logger"
 	"github.com/jabardigitalservice/portal-jabar-services/core-service/src/domain"
 	"github.com/labstack/echo/v4"
 	"github.com/mitchellh/mapstructure"
@@ -209,4 +211,30 @@ func GetMetaDataImage(link string) (meta domain.DetailMetaDataImage, err error) 
 	meta.Size = fileSize
 
 	return meta, err
+}
+
+func GetLog(c echo.Context) _goLog.LoggerData {
+	protocol := "http"
+	if c.Request().TLS != nil {
+		protocol = "https"
+	}
+	return _goLog.LoggerData{
+		AdditionalInfo: map[string]interface{}{
+			"http_host":         c.Request().Host,
+			"http_addr":         protocol + "://" + c.Request().Host + c.Request().RequestURI,
+			"http_method":       c.Request().Method,
+			"http_proto":        c.Request().Proto,
+			"http_schema":       protocol,
+			"http_uri":          c.Request().RequestURI,
+			"remote_addr":       c.Request().RemoteAddr,
+			"resp_status":       c.Response().Status,
+			"user_agent":        c.Request().UserAgent(),
+			"resp_bytes_length": c.Request().ContentLength,
+			"resp_elapsed_ms":   "0ms",
+			"ts":                time.Now(),
+		},
+		Category: "Router",
+		Duration: 0,
+		Method:   fmt.Sprintf("[%s] %s", c.Request().Method, c.Request().URL),
+	}
 }
